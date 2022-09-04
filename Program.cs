0f0043e4eb5e01411aa0d0a0bf9a3d57f69e6bc7
Ethan Webb <WEBBEC@ETSU.EDU>
2022-09-04 11:55:59 -0400

    question 1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using ConsoleTables;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 // #1 print the output of adding two numbers from user input
+Console.WriteLine("Hello!");
+
 int num1, num2;
 Console.Write("Enter the first number: ");
 num1 = Convert.ToInt32(Console.ReadLine());
@@ -11,18 +12,5 @@ num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter the second number: ");
 num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
 
-// #2 Multiply table
-int bigNumber;
-
-Console.Write("Enter the number you like to use ");
-num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Enter the number you would like to use for the multiplication table:");
-bigNumber = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 0; i < bigNumber; i++)
-{
-    Console.WriteLine($"{num1} * {i} = {num1 * i}");
-}
