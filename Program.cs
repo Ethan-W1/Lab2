68b4325d52dca2676fad5489a92d32947782f157
Ethan Webb <WEBBEC@ETSU.EDU>
2022-09-04 12:21:05 -0400

    question 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using ConsoleTables;
+
+// See https://aka.ms/new-console-template for more information
+
+// #1 print the output of adding two numbers from user input
+Console.WriteLine("Hello!");
+
+int num1, num2;
+Console.Write("Enter the first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
