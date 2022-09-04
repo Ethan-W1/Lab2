7603e2377287157b2a768e0a5c308d87512d464a
Ethan-W1 <72032146+Ethan-W1@users.noreply.github.com>
2022-09-04 11:23:42 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleTables;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// #1 print the output of adding two numbers from user input
+int num1, num2;
+Console.Write("Enter the first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+
+// #2 Multiply table
+int bigNumber;
+
+Console.Write("Enter the number you like to use ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the number you would like to use for the multiplication table:");
+bigNumber = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < bigNumber; i++)
+{
+    Console.WriteLine($"{num1} * {i} = {num1 * i}");
+}
