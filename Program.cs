06664e4d468aa094d0d3fca32397daf19d50705f
Ethan Webb <WEBBEC@ETSU.EDU>
2022-09-04 12:04:58 -0400

    question 3

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 using ConsoleTables;
 
-// #2 Multiply table
-int bigNumber;
+// #3 Output the number of bytes in memory
 
-Console.Write("Enter the number you like to use ");
-num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Enter the number you would like to use for the multiplication table:");
-bigNumber = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 0; i < bigNumber; i++)
-{
-    Console.WriteLine($"{num1} * {i} = {num1 * i}");
-}
+ConsoleTable table = new ConsoleTable("Type", "Byte(s) of Memory", "Min", "Max");
+table.AddRow("sbyte", sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
+table.AddRow("byte", sizeof(byte), byte.MinValue, byte.MaxValue);
+table.AddRow("short", sizeof(short), short.MinValue, short.MaxValue);
+table.AddRow("ushort", sizeof(ushort), ushort.MinValue, ushort.MaxValue);
+table.AddRow("int", sizeof(int), int.MinValue, int.MaxValue);
+table.AddRow("uint", sizeof(uint), uint.MinValue, uint.MaxValue);
+table.AddRow("long", sizeof(long), long.MinValue, long.MaxValue);
+table.AddRow("ulong", sizeof(ulong), ulong.MinValue, ulong.MaxValue);
+table.AddRow("float", sizeof(float), float.MinValue, float.MaxValue);
+table.AddRow("double", sizeof(double), double.MinValue, double.MaxValue);
+table.AddRow("decimal", sizeof(decimal), decimal.MinValue, decimal.MaxValue);
+Console.WriteLine(table);
